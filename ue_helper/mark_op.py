3bfe36995e7a797f33d5fabfd93ecd56fc3430be
Vertex Machine <vertexmachine@vertexrage.com>
2024-01-30 00:11:04 +0100

    Fixed selection handling during uv lightmap generation

--- ue_helper/mark_op.py
+++ ue_helper/mark_op.py
@@ -1,13 +1,14 @@
 import bpy
 
-from . utils import mark_selected
+from .utils import mark_selected, generate_lightmap_uvs
+
 
 class UEMark_OT_Operator(bpy.types.Operator):
     bl_idname = "uehelper.mark"
     bl_label = "Helper function for marking to export"
     bl_description = "Helper function for marking to export"
 
-
     def execute(self, context):
         mark_selected(True, context)
+        generate_lightmap_uvs(context)
         return {'FINISHED'}
--- ue_helper/utils.py
+++ ue_helper/utils.py
@@ -35,17 +35,42 @@ def mark_selected(for_export=True, context=None):
                 obj.bfu_additional_rotation_for_export.z = radians(90)
             else:
                 obj.bfu_additional_rotation_for_export.z = 0
-
-            if lightmap_gen is True:
-                generate_lightmap_uvs(obj)
-                obj.bfu_generate_light_map_uvs = False
-            else:
-                obj.bfu_generate_light_map_uvs = True
         else:
             obj.bfu_export_type = 'auto'  # Set not for exporting
 
 
-def generate_lightmap_uvs(obj):
+def generate_lightmap_uvs(context):
+    lightmap_gen = False
+    if context:
+        lightmap_gen = context.scene.ue_helper_blender_gen_lightmap_uvs
+
+    # Store current selection
+    selected_objects = bpy.context.selected_objects
+    old_active = bpy.context.view_layer.objects.active
+    bpy.ops.object.select_all(action='DESELECT')
+
+    # Iterate over selected objects
+    for obj in selected_objects:
+        if obj.type != 'MESH':
+            continue
+        obj.select_set(True)
+        bpy.context.view_layer.objects.active = obj
+        if lightmap_gen is True:
+            generate_lightmap_uvs_for_obj(obj)
+            obj.bfu_generate_light_map_uvs = False
+        else:
+            mesh = obj.data
+            if len(mesh.uv_layers) == 1:  # if it already had more than 1 UV map, don't mess with the setting
+                obj.bfu_generate_light_map_uvs = True
+        obj.select_set(False)
+
+    # Restore selection
+    bpy.context.view_layer.objects.active = old_active
+    for obj in selected_objects:
+        obj.select_set(True)
+
+
+def generate_lightmap_uvs_for_obj(obj):
     # Check if the object is a mesh
     if obj.type != 'MESH':
         print("The object is not a mesh. Skipping uv lightmap generation.")
@@ -62,7 +87,7 @@ def generate_lightmap_uvs(obj):
         # Generate lightmap UVs
         bpy.ops.object.mode_set(mode='EDIT')
         bpy.ops.mesh.select_all(action='SELECT')
-        bpy.ops.uv.lightmap_pack(PREF_CONTEXT='ALL_FACES',PREF_PACK_IN_ONE=False, PREF_BOX_DIV=30, PREF_MARGIN_DIV=0.2)
+        bpy.ops.uv.lightmap_pack(PREF_CONTEXT='ALL_FACES', PREF_PACK_IN_ONE=False, PREF_BOX_DIV=30, PREF_MARGIN_DIV=0.2)
         bpy.ops.object.mode_set(mode='OBJECT')
 
         # Reselect the original UV layer
